ddbf70c3800ea644ca440d0225b22bcfcd4b2628
Gabriel Velazquez <gvelazquez@riafinancial.com>
2020-04-28 13:20:38 -0700

    Show confirmation message after savig using temp Data

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -64,6 +64,7 @@ namespace OdeToFood.Pages.Restaurants
 			}
 
 			restaurantData.Commit();
+			TempData["Message"] = "Restaurant saved!";
 			return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
 		}
 	}
