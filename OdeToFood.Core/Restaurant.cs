a6a62fcc784295e899ff67fe0f0f21027713880a
Gabriel Velazquez <gvelazquez@riafinancial.com>
2020-04-28 12:59:39 -0700

    Restaurant edit functionality

--- OdeToFood.Core/Restaurant.cs
+++ OdeToFood.Core/Restaurant.cs
@@ -1,10 +1,15 @@
-﻿namespace OdeToFood.Core
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Core
 {
     public class Restaurant
     {
         public int Id { get; set; }
+        [Required, StringLength(30)]
         public string Name { get; set; }
+        [Required, StringLength(50)]
         public string Location { get; set; }
+        [Required]
         public CuisineType Cuisine { get; set; }
     }
 }
---
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+	public class EditModel : PageModel
+	{
+		private readonly IRestaurantData restaurantData;
+		private readonly IHtmlHelper htmlHelper;
+		[BindProperty]
+		public Restaurant Restaurant { get; set; }
+		public IEnumerable<SelectListItem> Cuisines { get; set; }
+
+		public EditModel(IRestaurantData restaurantData,
+			IHtmlHelper htmlHelper)
+		{
+			this.restaurantData = restaurantData;
+			this.htmlHelper = htmlHelper;
+		}
+
+		public IActionResult OnGet(int restaurantId)
+		{
+			Restaurant = restaurantData.GetById(restaurantId);
+			Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+
+			if (Restaurant == null)
+			{
+				return RedirectToPage("./NotFound");
+			}
+
+			return Page();
+		}
+
+		public IActionResult OnPost()
+		{
+			if (ModelState.IsValid)
+			{
+				Restaurant = restaurantData.Update(Restaurant);
+				restaurantData.Commit();
+				return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id });
+			}
+
+			Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+			return Page();
+		}
+	}
+}
